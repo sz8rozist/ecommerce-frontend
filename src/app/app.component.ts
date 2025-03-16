3b0682d4fe4bbeebfc9805d30c71645fb4725bc0
Rózsa István <ristvan98@gmail.com>
2025-03-16 21:15:33 +0100

    Angular material install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule, MatButtonModule, MatToolbarModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
